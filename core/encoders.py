0dbace53edb3e21abde6d041c602cdde526d7dff
Harry Alvarado <harry2alvarado8@hotmail.com>
2025-04-25 23:42:38 -0500

    add modules

---
+++ core/encoders.py
@@ -0,0 +1,3 @@
+def run():
+    print("\n[🔐] Módulo de Encoders aún está en construcción...")
+    print("Planeado para codificar/obfuscar payloads y evitar detección.\n")
---
+++ core/listeners.py
@@ -0,0 +1,3 @@
+def run():
+    print("\n[🎧] Módulo de Listeners aún está en construcción...")
+    print("En el futuro podrás esperar conexiones reversas desde tus payloads.\n")
